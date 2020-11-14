fac2a17a12cc9d89f004ebc1f3a129c40856c017
Cod3rBlack <cod3r.black@gmail.com>
2020-11-14 22:12:39 +0530

    1st update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
 
-class RegularClass{}
-class ComponentClass extends Component{}
-
-const regularClassInstance = new RegularClass();
-const componentClassInstance = new ComponentClass();
-
-
-console.log('regularClassInstance', regularClassInstance);
-console.log('componentClassInstance', componentClassInstance);
 
 class App extends Component{
+
+    state = { displayBio: false };
+
+    // constructor(){
+    //     super();
+    //     this.state = { displayBio : false };
+
+    //     // this.readMore = this.readMore.bind(this);
+    //     // this.showLess = this.showLess.bind(this);
+    //     this.toggleDispalyBio = this.toggleDispalyBio.bind(this);
+    // }
+
+    // readMore(){
+    //     this.setState({ displayBio: true });
+    // }
+
+    // showLess() {
+    //     this.setState({ displayBio : false});
+    // }
+
+    toggleDispalyBio = () => {
+        this.setState({ displayBio: !this.state.displayBio});
+    }
+
     render() {
         return (
             <div>
                 <h1> Hello</h1>
                 <p> My name is Vishwajeet Kumar.</p>
                 <p> i am a software developer and data analyst.</p>
+                {
+                    this.state.displayBio ? 
+                    (
+                        <div>
+                            <p>I am from Ara, Bihar, India.</p>
+                            <p>My favorite languages are Hindi and English.</p>
+                            <p>Other than coding , i also love playing Chess.</p>
+                            <button onClick = {this.toggleDispalyBio }>Show Less</button>
+                        </div>
+                    ) : (
+                        <div>
+                            <button onClick = {this.toggleDispalyBio }> Read More </button>
+                        </div>
+                    )
+                }
+
+                <hr />
+                <Projects />
+                <hr />
+                <SocialProfiles />
             </div>
         )
     }
